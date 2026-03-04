60e50c4710ba8cbe31376e52658b0823c4e4e316
Anton Samy <78489379+AntonSamy20@users.noreply.github.com>
2026-03-05 01:35:42 +0200

    Add solution for Move Zeroes (trial 1)

---
+++ leetcode/move-zeroes_trial1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        digit = 0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[i],nums[digit] = nums[digit],nums[i]
+                digit +=1
+            
+            
+        
