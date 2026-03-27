851badef289d53ebc8db7beb290dee9bcd88e3bd
Anton Samy <78489379+AntonSamy20@users.noreply.github.com>
2026-03-27 12:16:41 +0200

    Add solution for Fruit Into Baskets (trial 1)

--- leetcode/fruit-into-baskets_trial1.py
+++ leetcode/fruit-into-baskets_trial1.py
@@ -1,11 +1,18 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        baskets = {}
-        for i in range(len(fruits)):
-            baskets[fruits[i]] = baskets.get(fruits[i],0) + 1
-        
-        target = sorted(baskets.values())
+        n = len(fruits)
+        mx = 0
+        left = 0
 
-        return sum(target[-2:])
+        seen = {}
+        for i in range(n): 
+            seen[fruits[i]] = seen.get(fruits[i], 0) + 1
 
-        
+            while len(seen) > 2:
+                seen[fruits[left]]-=1
+                if seen[fruits[left]]==0:
+                    del seen[fruits[left]]
+                left+=1
+            mx = max(mx, i-left+1)
+
+        return mx    
